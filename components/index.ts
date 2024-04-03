9c17057a82f8df6b51789c8ea186ef58ac8b5127
Vishrut8898 <vishrut9869@gmail.com>
2024-04-03 21:16:50 +0530

    feat: Car Grid and Filters

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,22 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import CustomFilters from "./CustomFilters";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
 
 export {
     Hero,
     CustomButton,
     Navbar,
     Footer,
+    CustomFilters,
+    SearchBar,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore,
 }
